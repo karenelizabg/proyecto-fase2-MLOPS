799acd15b9e91c0e7b3dc8b172ee7fdf3d2e4f90
Lanachiza <79599095+Lanachiza@users.noreply.github.com>
2026-09-12 16:42:30 -0600

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = var.github_actions_role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = data.aws_iam_openid_connect_provider.github.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:*"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    ManagedBy = "Terraform"
+    Project   = "proyecto-fase2-MLOPS"
+  }
+}
+
+output "github_actions_role_arn" {
+  description = "ARN del IAM Role que debe asumir GitHub Actions."
+  value       = aws_iam_role.github_actions.arn
+}
