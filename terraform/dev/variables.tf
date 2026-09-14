09beb54368010b2d6e00455a31ffc6c3a1840faf
Lanachiza <79599095+Lanachiza@users.noreply.github.com>
2026-09-14 15:03:12 -0600

    Create variables.tf

---
+++ terraform/dev/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "Región de AWS."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "availability_zone" {
+  description = "Zona de disponibilidad para dev."
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "ami_id" {
+  description = "AMI para la instancia EC2."
+  type        = string
+  default     = "ami-0c02fb55956c7d316"
+}
+
+variable "db_username" {
+  description = "Usuario de la base de datos."
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Contraseña de la base de datos."
+  type        = string
+  sensitive   = true
+}
+
+variable "bucket_name" {
+  description = "Nombre del bucket de dev."
+  type        = string
+  default     = "proyecto-fase2-mlops-dev-storage"
+}
