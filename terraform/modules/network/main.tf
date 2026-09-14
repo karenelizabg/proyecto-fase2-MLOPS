f505fbda79e4fce7c53ce19c4e086e74f10ed43e
Lanachiza <79599095+Lanachiza@users.noreply.github.com>
2026-09-14 13:59:33 -0600

    Create main.tf

---
+++ terraform/modules/network/main.tf
@@ -0,0 +1,22 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "${var.environment}-vpc"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.environment}-public-subnet"
+    Environment = var.environment
+  }
+}
