b81de0190a8efeec78dfd8df38aee245c23bf546
Lanachiza <79599095+Lanachiza@users.noreply.github.com>
2026-09-14 15:00:55 -0600

    Create variables.tf

---
+++ terraform/modules/storage/variables.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+  description = "Nombre del entorno."
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Nombre único del bucket."
+  type        = string
+}
